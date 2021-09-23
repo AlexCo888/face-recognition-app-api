573554125d0906527a6ff2dc0430e9a1ca6c122e
JuanB2019 <contacto@cafedelossentidos>
2021-09-23 00:01:48 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(db.users);
+});
+
+// SIGN IN
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+
+// REGISTER
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+// PROFILE/:ID
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+
+// IMAGE
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
+
+/* 
+    / --> res = this is working -->
+    /singin --> POST = success/fail
+    /register --> POST = user
+    /profile/:userID --> GET = user
+    /image --> PUT --> user
+
+*/
